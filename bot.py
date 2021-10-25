3c119c7aaa85e6908f6f90931e1d50929e83e52f
Carlos Soeima <carlos_paredes_1996@hotmail.com>
2021-10-25 19:31:05 +0100

    opus added

--- bot.py
+++ bot.py
@@ -7,6 +7,13 @@ import youtube_dl
 from discord import FFmpegPCMAudio
 
 load_dotenv()
+if not discord.opus.is_loaded():
+    # the 'opus' library here is opus.dll on windows
+    # or libopus.so on linux in the current directory
+    # you should replace this with the location the
+    # opus library is located in and with the proper filename.
+    # note that on windows this DLL is automatically provided for you
+    discord.opus.load_opus('opus')
 
 client = discord.Client()
 
